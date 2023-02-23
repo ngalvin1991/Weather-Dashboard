c97656ec25d33dac319da2b1823f79b7a5beaa10
ngalvin1991 <117473760+ngalvin1991@users.noreply.github.com>
2023-02-23 18:50:52 +0000

    added bootstrap stylesheet and js script

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// AS A traveler
+// I WANT to see the weather outlook for multiple cities
+// SO THAT I can plan a trip accordingly
+
+    var apiKey = "2aa16dce274982742e5ffab3905871cd";
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}";
+    var searchBtn = $("#search-button");
+
+  
